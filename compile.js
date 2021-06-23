983487f63ff6e43230e2f872fe0ec310d986fefb
Joshua Sacuedo <joshuansu0897@gmail.com>
2021-06-23 18:19:59 -0400

    print

--- compile.js
+++ compile.js
@@ -1,1 +1,29 @@
 'use strict'
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
+const { CLIENT_RENEG_LIMIT } = require('tls')
+
+const InboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
+const contentInboxFile = fs.readFileSync(InboxPath, 'utf8')
+
+const input = {
+  language: 'Solidity',
+  sources: {
+    'Inbox.sol': {
+      content: contentInboxFile
+    }
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      }
+    }
+  }
+}
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)))
+
+console.log(JSON.stringify(output, null, 4))
+console.log(output);
