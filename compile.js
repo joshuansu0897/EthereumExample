57fcc93f52496e3733474070ee0f1802a2250c34
Joshua Sacuedo <joshuansu0897@gmail.com>
2021-06-23 18:28:58 -0400

    unnecesary

--- compile.js
+++ compile.js
@@ -2,7 +2,6 @@
 const path = require('path')
 const fs = require('fs')
 const solc = require('solc')
-const { CLIENT_RENEG_LIMIT } = require('tls')
 
 const InboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
 const contentInboxFile = fs.readFileSync(InboxPath, 'utf8')
