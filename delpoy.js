79a4f962f833cb7d5f68abb5cfde7cf02caf3ac2
Joshua Sacuedo <joshuansu0897@gmail.com>
2021-06-23 21:24:36 -0400

    adding deplotment to rinkbey and env variables

--- delpoy.js
+++ delpoy.js
@@ -1,1 +1,32 @@
 'use strict'
+require('dotenv').config()
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const Web3 = require("web3")
+const Inbox = require('./compile').Inbox
+
+const mnemonicPhrase = process.env.WORDS
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID
+
+const provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: mnemonicPhrase
+  },
+  providerOrUrl: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`
+})
+
+const web3 = new Web3(provider)
+
+
+async function deploy() {
+  const accounts = await web3.eth.getAccounts()
+
+  console.log(`should be my account ${accounts[0]}`)
+
+  const contractInbox = await new web3.eth.Contract(Inbox.abi)
+    .deploy({ data: Inbox.evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' })
+
+  console.log(`Contract deployed to ${contractInbox.options.address}`)
+}
+
+deploy()
